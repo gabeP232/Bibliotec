c2005cdb188ae81d247bb3881531d3720695832f
airsquared <36649395+airsquared@users.noreply.github.com>
2024-04-28 00:59:40 -0700

    Add tables to homepage

--- src/main/java/org/bibliotec/app/HomeController.java
+++ src/main/java/org/bibliotec/app/HomeController.java
@@ -1,16 +1,31 @@
 package org.bibliotec.app;
 
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 
 public class HomeController {
 
     private static Scene scene;
 
-    public static Scene getScene() {
+    @SuppressWarnings("rawtypes") @FXML private TableView booksTable, loansTable;
+    @FXML private ToggleGroup tabs;
+
+    public static void show() {
         if (scene == null) {
             try {
                 scene = new Scene(FXMLLoader.load(HomeController.class.getResource("home.fxml")));
@@ -18,19 +33,61 @@ public class HomeController {
                 throw new UncheckedIOException(e);
             }
         }
-        return scene;
+        Main.stage.setScene(scene);
+    }
+
+    @SuppressWarnings("unchecked")
+    public void initialize() {
+        tabs.selectedToggleProperty().addListener((__, oldVal, selected) -> {
+            if (selected == null) {
+                oldVal.setSelected(true);
+            } else {
+                var childs = ((VBox) scene.getRoot()).getChildren();
+                childs.set(childs.size() - 1, (Node) selected.getUserData());
+            }
+        });
+        columnsFromRecord(booksTable, DatabaseAccess.Book.class,
+                Map.of("title", "Title", "author", "Author", "isbn", "ISBN", "genre", "Genre", "publisher", "Publisher", "year", "Year", "pages", "Pages"));
+        booksTable.setItems(FXCollections.observableArrayList(DatabaseAccess.getBooks()));
+    }
+
+    private static final Object placeholder = new Object();
+
+    private static<R extends Record> void columnsFromRecord(TableView<R> table, Class<R> record, Map<String, String> columnNames) {
+        for (var component : record.getRecordComponents()) {
+            var column = new TableColumn<R, String>(columnNames.getOrDefault(component.getName(), component.getName()));
+            column.setCellValueFactory(cellData -> {
+                try {
+                    if (cellData.getValue() == null || cellData.getValue() == placeholder) {
+                        return new ReadOnlyStringWrapper("<edit>");
+                    }
+                    return new ReadOnlyStringWrapper(String.valueOf(component.getAccessor().invoke(cellData.getValue())));
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            column.setOnEditCommit(event -> {
+                String newValue = event.getNewValue();
+                R recordInstance = event.getRowValue();
+                // save the data
+            });
+            column.setCellFactory(TextFieldTableCell.forTableColumn());
+            table.getColumns().add(column);
+        }
     }
 
     public void logout() {
+        LoginController.show();
     }
 
-    public void books() {
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public void add(ActionEvent actionEvent) {
+        ((TableView) ((Node) actionEvent.getTarget()).getUserData()).getItems().add(placeholder);
     }
 
-    public void users() {
-    }
-
-    public void loans() {
-
+    @SuppressWarnings("rawtypes")
+    public void delete(ActionEvent actionEvent) {
+        var table = ((TableView) ((Node) actionEvent.getTarget()).getUserData());
+        table.getItems().remove(table.getSelectionModel().getSelectedItem());
     }
 }
--- src/main/java/org/bibliotec/app/LoginController.java
+++ src/main/java/org/bibliotec/app/LoginController.java
@@ -3,46 +3,111 @@ package org.bibliotec.app;
 import atlantafx.base.controls.Message;
 import atlantafx.base.theme.Styles;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.List;
 
 import static javafx.beans.binding.Bindings.when;
 
 public class LoginController {
 
-    public static final String USERNAME = "admin";
-    public static final String PASSWORD = "admin";
+    private static Scene scene;
 
-    public TextField username;
-    public PasswordField password;
-    public Message message;
+    @FXML private TextField username;
+    @FXML private PasswordField password, confirmPassword;
+    @FXML private Message errorMessage;
+    @FXML private Button login, register;
+    private final SimpleBooleanProperty registering = new SimpleBooleanProperty();
+
+    public static void show() {
+        if (scene == null) {
+            try {
+                scene = new Scene(FXMLLoader.load(LoginController.class.getResource("login.fxml")));
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+        Main.stage.setScene(scene);
+    }
 
     public void initialize() {
-        message.managedProperty().bind(message.visibleProperty());
-        message.descriptionProperty().bind(Bindings.concat(
-                "Invalid Credentials.",
-                when(username.textProperty().isEmpty().or(password.textProperty().isEmpty())).then("\n").otherwise(""),
-                when(username.textProperty().isEmpty()).then("Username is required.").otherwise(""),
-                when(username.textProperty().isEmpty().and(password.textProperty().isEmpty())).then("\n").otherwise(""),
-                when(password.textProperty().isEmpty()).then("Password is required.").otherwise("")
-        ));
+        registering.addListener(__ -> {
+            errorMessage.setVisible(false);
+            List.of(username, password, confirmPassword).forEach(node -> setRedHighlight(node, false));
+        });
+        register.textProperty().bind(when(registering).then("Confirm Registration").otherwise("Register"));
+        login.textProperty().bind(when(registering).then("Cancel").otherwise("Login"));
+        confirmPassword.visibleProperty().bind(registering);
+        confirmPassword.managedProperty().bind(confirmPassword.visibleProperty());
+        errorMessage.managedProperty().bind(errorMessage.visibleProperty());
+        var usernameInvalid = username.textProperty().length().lessThan(4);
+        var passwordInvalid = password.textProperty().length().lessThan(8);
+        var confirmPasswordInvalid = confirmPassword.textProperty().isNotEqualTo(password.textProperty());
+
+        errorMessage.descriptionProperty().bind(
+                when(registering).then(Bindings.concat(
+                        when(usernameInvalid).then("Username must be at least 4 characters.").otherwise(""),
+                        when(usernameInvalid.and(passwordInvalid)).then("\n").otherwise(""),
+                        when(passwordInvalid).then("Password must be at least 8 characters.").otherwise(""),
+                        when(usernameInvalid.or(passwordInvalid).and(confirmPasswordInvalid)).then("\n").otherwise(""),
+                        when(confirmPasswordInvalid).then("Passwords must match.").otherwise("")
+                )).otherwise(
+                        "Invalid Credentials."
+                ));
     }
 
     public void login() {
-        if (username.getText().equals(USERNAME) && password.getText().equals(PASSWORD)) {
-            message.setVisible(false);
-            homepage();
+        if (registering.get()) {
+            registering.set(false);
             return;
         }
-        username.pseudoClassStateChanged(Styles.STATE_DANGER, true);
-        password.pseudoClassStateChanged(Styles.STATE_DANGER, true);
-        message.setVisible(true);
+
+        if (DatabaseAccess.login(username.getText(), password.getText())) {
+            errorMessage.setVisible(false);
+            HomeController.show();
+        } else {
+            errorMessage.setVisible(true);
+        }
     }
 
     public void register() {
+        if (registering.get()) {
+            boolean failed = false;
+            if (username.getText().length() < 4) {
+                setRedHighlight(username, true);
+                failed = true;
+            }
+            if (password.getText().length() < 8) {
+                setRedHighlight(password, true);
+                failed = true;
+            }
+            if (!confirmPassword.getText().equals(password.getText())) {
+                setRedHighlight(confirmPassword, true);
+                failed = true;
+            }
+            errorMessage.setVisible(failed);
+            if (!failed) {
+//                DatabaseAccess.addUser(new DatabaseAccess.User(username.getText(), password.getText()));
+                System.out.println("Registered: " + username.getText() + " with password: " + password.getText());
+                registering.set(false);
+                HomeController.show();
+            }
+        } else {
+            registering.set(true);
+        }
     }
 
-    private void homepage() {
-        Main.stage.setScene(HomeController.getScene());
+    private static void setRedHighlight(Node node, boolean highlight) {
+        node.pseudoClassStateChanged(Styles.STATE_DANGER, highlight);
     }
+
 }
