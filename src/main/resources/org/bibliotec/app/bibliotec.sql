03b86e87ff2a5485bdbb35d0607dcdb56c8cb5ba
airsquared <36649395+airsquared@users.noreply.github.com>
2024-04-30 19:17:38 -0700

    new SQL file

--- src/main/resources/org/bibliotec/app/bibliotec.sql
+++ src/main/resources/org/bibliotec/app/bibliotec.sql
@@ -2,83 +2,78 @@ CREATE DATABASE IF NOT EXISTS bibliotec;
 CREATE SCHEMA IF NOT EXISTS bibliotec;
 USE bibliotec;
 
+CREATE TABLE IF NOT EXISTS genre (
+                                     name VARCHAR(45) NOT NULL,
+                                     category VARCHAR(45) NULL DEFAULT NULL,
+                                     PRIMARY KEY (name));
+
+-- Java record
+-- public record Genre(String name, String category) {}
+
 CREATE TABLE IF NOT EXISTS books (
   bookName VARCHAR(45) NOT NULL,
   author VARCHAR(45) NOT NULL,
   isbn VARCHAR(45) NOT NULL,
   publisher VARCHAR(45) NOT NULL,
-  PRIMARY KEY (isbn));
+  genre VARCHAR(45) NOT NULL,
+  totalCopies INT DEFAULT 1,
+  PRIMARY KEY (isbn),
+  FOREIGN KEY (genre) REFERENCES genre (name));
+
+-- Java record
+-- public record Book(String bookName, String author, String isbn, String publisher, String genre, int totalCopies) {}
 
 
-CREATE TABLE IF NOT EXISTS patrons (
+CREATE TABLE IF NOT EXISTS users (
   name VARCHAR(45) NOT NULL,
   phoneNum VARCHAR(45) NOT NULL,
   address VARCHAR(45) NOT NULL,
-  patronID INT NOT NULL,
-  PRIMARY KEY (patronID));
+  userID VARCHAR(45) NOT NULL,
+  password VARCHAR(45) NOT NULL,
+  isAdmin BOOLEAN NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (userID));
 
+-- Java record
+-- public record User(String name, String phoneNum, String address, String userID, String userPassword, boolean isAdmin) {}
 
 CREATE TABLE IF NOT EXISTS checkout (
   checkoutID INT NOT NULL,
-  bookName VARCHAR(45) NOT NULL,
-  patronID INT NOT NULL,
-  returnDate VARCHAR(45) NOT NULL,
+  isbn VARCHAR(50) NOT NULL,
+  userID VARCHAR(45) NOT NULL,
+  checkoutDate DATE NOT NULL,
+  expectedReturnDate DATE NOT NULL,
+  returned BOOLEAN,
   PRIMARY KEY (checkoutID),
-  INDEX patronID_idx (patronID ASC) VISIBLE,
-  CONSTRAINT patronID_fk
-    FOREIGN KEY (patronID)
-    REFERENCES bibliotec.patrons (patronID));
+  FOREIGN KEY (userID) REFERENCES users (userID));
 
-
-CREATE TABLE IF NOT EXISTS genre (
-  type VARCHAR(45) NOT NULL,
-  author_fk VARCHAR(45) NOT NULL,
-  subtype VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (type),
-  INDEX author_fk_idx (author_fk ASC) VISIBLE,
-  CONSTRAINT author_fk
-    FOREIGN KEY (author_fk)
-    REFERENCES books (isbn));
-
-
-
-CREATE TABLE IF NOT EXISTS login (
-  username TEXT NOT NULL,
-  password TEXT NOT NULL);
+-- Java record
+-- public record Checkout(int checkoutID, String isbn, String userID, Date checkoutDate, Date expectedReturnDate, boolean returned) {}
 
 -- Inserting data into the tables
-INSERT IGNORE INTO books (bookName, author, isbn, publisher) VALUES ('The Great Gatsby', 'F. Scott Fitzgerald', '9780743273565', 'Scribner');
-INSERT IGNORE INTO books (bookName, author, isbn, publisher) VALUES ('To Kill a Mockingbird', 'Harper Lee', '9780061120084', 'Harper Perennial Modern Classics');
-INSERT IGNORE INTO books (bookName, author, isbn, publisher) VALUES ('1984', 'George Orwell', '9780451524935', 'Signet Classic');
-INSERT IGNORE INTO books (bookName, author, isbn, publisher) VALUES ('Pride and Prejudice', 'Jane Austen', '9780679783268', 'Modern Library');
-INSERT IGNORE INTO books (bookName, author, isbn, publisher) VALUES ('The Catcher in the Rye', 'J.D. Salinger', '9780316769488', 'Little, Brown and Company');
-INSERT IGNORE INTO books (bookName, author, isbn, publisher) VALUES ('The Hobbit', 'J.R.R. Tolkien', '9780345339683', 'Del Rey');
-INSERT IGNORE INTO books (bookName, author, isbn, publisher) VALUES ('Fahrenheit 451', 'Ray Bradbury', '9781451673319', 'Simon & Schuster');
-INSERT IGNORE INTO books (bookName, author, isbn, publisher) VALUES ('The Lord of the Rings', 'J.R.R. Tolkien', '9780618640157', 'Houghton Mifflin Harcourt');
+INSERT INTO genre (name, category) VALUES ('Fantasy', 'Fiction');
+INSERT INTO genre (name, category) VALUES ('Science Fiction', 'Fiction');
+INSERT INTO genre (name, category) VALUES ('Mystery', 'Fiction');
+INSERT INTO genre (name, category) VALUES ('Romance', 'Fiction');
+INSERT INTO genre (name, category) VALUES ('Reference', 'Non-Fiction');
+INSERT INTO genre (name, category) VALUES ('Biography', 'Non-Fiction');
+INSERT INTO genre (name, category) VALUES ('Self-Help', 'Non-Fiction');
 
-INSERT IGNORE INTO patrons (name, phoneNum, address, patronID) VALUES ('John Doe', '123-456-7890', '123 Main St', 1);
-INSERT IGNORE INTO patrons (name, phoneNum, address, patronID) VALUES ('Jane Doe', '123-456-7890', '123 Main St', 2);
-INSERT IGNORE INTO patrons (name, phoneNum, address, patronID) VALUES ('Alice Smith', '123-456-7890', '123 Main St', 3);
-INSERT IGNORE INTO patrons (name, phoneNum, address, patronID) VALUES ('Bob Smith', '123-456-7890', '123 Main St', 4);
-INSERT IGNORE INTO patrons (name, phoneNum, address, patronID) VALUES ('Charlie Brown', '123-456-7890', '123 Main St', 5);
-INSERT IGNORE INTO patrons (name, phoneNum, address, patronID) VALUES ('Lucy Brown', '123-456-7890', '123 Main St', 6);
-INSERT IGNORE INTO patrons (name, phoneNum, address, patronID) VALUES ('Snoopy', '123-456-7890', '123 Main St', 7);
-INSERT IGNORE INTO patrons (name, phoneNum, address, patronID) VALUES ('Linus', '123-456-7890', '123 Main St', 8);
+INSERT INTO books (bookName, author, isbn, publisher, genre) VALUES ('Harry Potter and the Sorcerer''s Stone', 'J.K. Rowling', '978-0439708180', 'Scholastic', 'Fantasy');
+INSERT INTO books (bookName, author, isbn, publisher, genre) VALUES ('The Hobbit', 'J.R.R. Tolkien', '978-0345534835', 'Houghton Mifflin Harcourt', 'Fantasy');
+INSERT INTO books (bookName, author, isbn, publisher, genre) VALUES ('The Hunger Games', 'Suzanne Collins', '978-0439023481', 'Scholastic', 'Science Fiction');
+INSERT INTO books (bookName, author, isbn, publisher, genre) VALUES ('Divergent', 'Veronica Roth', '978-0062024039', 'Katherine Tegen Books', 'Science Fiction');
+INSERT INTO books (bookName, author, isbn, publisher, genre) VALUES ('The Da Vinci Code', 'Dan Brown', '978-0307474278', 'Anchor', 'Mystery');
+INSERT INTO books (bookName, author, isbn, publisher, genre) VALUES ('English Dictionary', 'Merriam-Webster', '978-0877792956', 'Merriam-Webster', 'Reference');
+INSERT INTO books (bookName, author, isbn, publisher, genre) VALUES ('Steve Jobs', 'Walter Isaacson', '978-1451648539', 'Simon & Schuster', 'Biography');
+INSERT INTO books (bookName, author, isbn, publisher, genre) VALUES ('The 7 Habits of Highly Effective People', 'Stephen R. Covey', '978-1982137274', 'Simon & Schuster', 'Self-Help');
 
-INSERT IGNORE INTO checkout (checkoutID, bookName, patronID, returnDate) VALUES (1, 'The Great Gatsby', 1, '2021-12-31');
-INSERT IGNORE INTO checkout (checkoutID, bookName, patronID, returnDate) VALUES (2, 'To Kill a Mockingbird', 2, '2021-12-31');
-INSERT IGNORE INTO checkout (checkoutID, bookName, patronID, returnDate) VALUES (3, '1984', 3, '2021-12-31');
-INSERT IGNORE INTO checkout (checkoutID, bookName, patronID, returnDate) VALUES (4, 'Pride and Prejudice', 4, '2021-12-31');
-INSERT IGNORE INTO checkout (checkoutID, bookName, patronID, returnDate) VALUES (5, 'The Catcher in the Rye', 5, '2021-12-31');
-INSERT IGNORE INTO checkout (checkoutID, bookName, patronID, returnDate) VALUES (6, 'The Hobbit', 6, '2021-12-31');
-INSERT IGNORE INTO checkout (checkoutID, bookName, patronID, returnDate) VALUES (7, 'Farenheit 451', 5, '2021-12-31');
--- add multiple checkouts for each patron
-INSERT IGNORE INTO checkout (checkoutID, bookName, patronID, returnDate) VALUES (8, 'The Lord of the Rings', 1, '2021-12-31');
-INSERT IGNORE INTO checkout (checkoutID, bookName, patronID, returnDate) VALUES (9, 'The Lord of the Rings', 2, '2021-12-31');
+INSERT INTO users (name, phoneNum, address, userID, password) VALUES ('John Doe', '123-456-7890', '123 Main St', 'johndoe', 'password');
+INSERT INTO users (name, phoneNum, address, userID, password) VALUES ('Jane Doe', '123-456-7890', '123 First St', 'janedoe', 'password');
+INSERT INTO users (name, phoneNum, address, userID, password) VALUES ('Bob Smith', '123-456-7890', '123 Second St', 'bobsmith', 'password');
+INSERT INTO users (name, phoneNum, address, userID, password) VALUES ('James Stewart', '123-456-7890', '123 Third St', 'jstewart', 'password');
 
-
-INSERT IGNORE INTO genre (type, author_fk, subtype) VALUES ('Fiction', '9780743273565', 'Classic');
-INSERT IGNORE INTO genre (type, author_fk, subtype) VALUES ('Fiction', '9780061120084', 'Classic');
-INSERT IGNORE INTO genre (type, author_fk, subtype) VALUES ('Fiction', '9780451524935', 'Classic');
-INSERT IGNORE INTO genre (type, author_fk, subtype) VALUES ('Fiction', '9780679783268', 'Classic');
-
+INSERT INTO checkout (checkoutID, isbn, userID, checkoutDate, expectedReturnDate, returned) VALUES (1, '978-0439708180', 'johndoe', '2021-01-01', '2021-01-15', FALSE);
+INSERT INTO checkout (checkoutID, isbn, userID, checkoutDate, expectedReturnDate, returned) VALUES (2, '978-0345534835', 'janedoe', '2021-01-01', '2021-01-15', FALSE);
+INSERT INTO checkout (checkoutID, isbn, userID, checkoutDate, expectedReturnDate, returned) VALUES (3, '978-0439023481', 'bobsmith', '2021-01-01', '2021-01-15', FALSE);
+INSERT INTO checkout (checkoutID, isbn, userID, checkoutDate, expectedReturnDate, returned) VALUES (4, '978-0062024039', 'jstewart', '2021-01-01', '2021-01-15', FALSE);
+INSERT INTO checkout (checkoutID, isbn, userID, checkoutDate, expectedReturnDate, returned) VALUES (5, '978-0307474278', 'johndoe', '2021-01-01', '2021-01-15', FALSE);
